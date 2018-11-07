df9fae4e399286e58839513021c9fa4e51b1534e
Ricardo Mejia <coprovasc@aol.com>
2018-11-06 19:28:40 -0600

    Initial commit

---
+++ Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var str = "Tarea Velocimetro Digital"
+
+enum Velocidades : Int {
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    init( velocidadinicial : Velocidades) {
+        self = velocidadinicial
+    }
+}
